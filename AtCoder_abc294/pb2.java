54d0ab7e62ae6b945e2feeca598d9f9dc2d28ca5
Nikhil Garg <nikhilgarg.drb@gmail.com>
2023-03-20 22:48:42 +0530

    Update pb2.java

--- AtCoder_abc294/pb2.java
+++ AtCoder_abc294/pb2.java
@@ -1,9 +1,33 @@
 package AtCoder_abc294;
 
+//   https://atcoder.jp/contests/abc294/tasks/abc294_b
+
+import java.util.*;
+
 public class pb2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int x = scn.nextInt();
+		int y = scn.nextInt();
+		int arr[][] = new int[x][y];
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				arr[i][j] = scn.nextInt();
+			}
+		}
+
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				if (arr[i][j] == 0) {
+					System.out.print(".");
+				} else {
+					System.out.print((char) (arr[i][j] + 64));
+
+				}
+			}
+			System.out.println();
+		}
 
 	}
 
