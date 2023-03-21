3e3ac4e6a6923aff98d82ecbe9b02667f7fcf3cf
Nikhil Garg <nikhilgarg.drb@gmail.com>
2023-03-21 23:47:21 +0530

    Create pb3.java

---
+++ AtCoder_abc294/pb3.java
@@ -0,0 +1,48 @@
+package AtCoder_abc294;
+
+import java.util.*;
+
+public class pb3 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int qq = scn.nextInt();
+		int ww = scn.nextInt();
+		int arr1[] = new int[qq];
+		int arr2[] = new int[ww];
+		int arr[] = new int[qq + ww];
+		for (int i = 0; i < qq; i++) {
+			arr1[i] = scn.nextInt();
+			arr[i] = arr1[i];
+		}
+
+		for (int i = 0; i < ww; i++) {
+			arr2[i] = scn.nextInt();
+			arr[i + qq] = arr2[i];
+		}
+
+		Arrays.sort(arr);
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < qq + ww; i++) {
+			map.put(arr[i], i + 1);
+		}
+
+		for (int i = 0; i < qq; i++) {
+			arr1[i] = map.get(arr1[i]);
+		}
+		for (int i = 0; i < ww; i++) {
+			arr2[i] = map.get(arr2[i]);
+		}
+
+		for (int i : arr1) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		for (int i : arr2) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
