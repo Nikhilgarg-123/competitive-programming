5cf8e6d6f3f9c91db0df65baac0ee55a768c9afe
Nikhil Garg <nikhilgarg.drb@gmail.com>
2023-03-29 01:13:10 +0530

    Update graphs.java

--- graph_learning/graphs.java
+++ graph_learning/graphs.java
@@ -191,7 +191,7 @@ public class graphs {
 			// check koi dubara toh nahi aara means jaise kisi mai cycle hai
 //			toh ek element hi do baar aa skta     
 
-//				 a
+//		    	 a
 //				/ \          jaise  ider a to d hai toh  abd  and acd
 //			   b   c         2 raste bane toh isliye check karna padega
 //			    \  /
