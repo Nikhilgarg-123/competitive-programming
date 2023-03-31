d29566b13272c334cfe84116a1db8f7f158b356d
Nikhil Garg <nikhilgarg.drb@gmail.com>
2023-04-01 00:22:47 +0530

    Create str_to_int.java

---
+++ Recursion/str_to_int.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class str_to_int {
+
+	
+	public static void main(String[] args) {
+		
+		String str="123332";
+		int s=Integer.parseInt(str);
+		System.out.println(s);
+	}
+
+}
