20cc4c25141524942bf4e2ad6b50e686e42fde07
Nikhil Garg <nikhilgarg.drb@gmail.com>
2023-04-03 15:34:00 +0530

    Update pb4.java

--- atcoder296/pb4.java
+++ atcoder296/pb4.java
@@ -4,31 +4,28 @@ import java.util.*;
 
 public class pb4 {
 	// https://atcoder.jp/contests/abc296/tasks/abc296_d
-// abhi shi nahi hai
+
 	public static void main(String[] args) {
 
 		Scanner scn = new Scanner(System.in);
 
 		long n = scn.nextLong();
 		long x = scn.nextLong();
+		long s=1;
+		long e=n;
+		long ans=-1;
+		for(int i=1;i<=n;i++) {
+			long mul=s*e;
+			if(mul>=x) {
+				ans=mul;
+				e--;
+			}else {
+				s++;
+			}
+			
+		}
+		System.out.println(ans);
 
-		long a = x / n;
-		if (a * n < x) {
-			a++;
-		}
-		if (a > n) {
-			System.out.println(-1);
-			System.exit(0);
-		} else {
-			long b = x / a;
-			if (b * a < x) {
-				b++;
-			}
-			if (b > n) {
-				System.out.println(-1);
-				System.exit(0);
-			}
-			System.out.println(a * b);
-		}
+		
 	}
 }
